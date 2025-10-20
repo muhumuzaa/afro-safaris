68521b3d2ae815e21366b443cf30c4ceed78aad1
Ambroze AK <muhumuzaamb@gmail.com>
2025-10-19 23:31:27 -0400

    Add JUnit test for UserInputController. Also tests my custom exception - InvalidInputException

--- src/test/java/afro_safaris/util/UserInputControllerTest.java
+++ src/test/java/afro_safaris/util/UserInputControllerTest.java
@@ -1,5 +1,40 @@
 package afro_safaris.util;
 
+import org.junit.jupiter.api.Test;
+
+import afro_safaris.exception.InvalidInputException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.*;
+
+
+//tests for the userInput utility
 public class UserInputControllerTest {
 
+	//test 1. right int input
+	@Test
+	void testReadIntInputValidNumber() {
+		Scanner scanner = new Scanner("5\n");
+		int value = UserInputController.readIntInput(scanner, 1, 10, "Enter an number");
+		assertEquals(5, value);
+	}
+	
+	
+	//test2 - user enters out f range number. eg 15
+	@Test
+	void testReadIntInputThrowsRangeError() {
+		Scanner scanner = new Scanner("15\n");
+		InvalidInputException ex = assertThrows(InvalidInputException.class, () -> UserInputController.readIntInput(scanner, 1, 10, "Enter a number"));
+		assertEquals(InvalidInputException.ErrorType.RANGE_ERROR, ex.getErrorType());
+	}
+	
+//test3 - user enters mismatch input. e.g "abc"
+	@Test
+	void testReadIntputThrowsFormatError() {
+		Scanner scanner = new Scanner("15\n");
+		InvalidInputException ex = assertThrows(InvalidInputException.class, () -> UserInputController.readIntInput(scanner, 1, 10, "Enter a number:"));
+		assertEquals(InvalidInputException.ErrorType.FORMART_ERROR, ex.getErrorType());
+	}
 }
