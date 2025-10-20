f97239d4c430640da387c30d56421056bc5bff06
Ambroze AK <muhumuzaamb@gmail.com>
2025-10-20 01:16:53 -0400

    Add tests for packageCost() on luxury and normal safariPackage

---
+++ src/test/java/afro_safaris/model/LuxurySafariPackageTest.java
@@ -0,0 +1,35 @@
+package afro_safaris.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class LuxurySafariPackageTest {
+	
+	//this test is for polymorphism: packageCost works different on standard and luxury packages
+	@Test
+	void testLuxuryPackageExtraCost() {
+		Destination dstn = new Destination("Luxury destn", "luxury place", 200.0);
+		SafariPackage lux = new LuxurySafariPackkage(dstn, 3); //reference type is parent, object is child
+		
+		//normal price should be 3*200 = 600.
+		//luxury adds 20% = 720
+		assertEquals(720, lux.getPackageCost(), 0.01);
+	}
+	
+	
+	//testing polymorphism- getPackageCost() acts different on the 2 types of SafariPackages
+	@Test
+	void testLuxuryPackagePolymorphism() {
+		Destination dstn = new Destination("Luxury destn", "luxury place", 200.0);
+		SafariPackage lux = new LuxurySafariPackkage(dstn, 2);
+		
+		double cost = lux.getPackageCost(); 
+		
+		//normal price should be 2*200 = 400.
+				//luxury adds 20% = 480
+		assertEquals(480, cost, 0.01);
+	}
+
+}
